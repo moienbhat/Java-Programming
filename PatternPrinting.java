7370c81bda0c7ab07abf628a66b346b0da2210cc
Moien Bhat <bhatmoien34@gmail.com>
2026-04-20 22:05:05 +0530

    Add files via upload

---
+++ PatternPrinting.java
@@ -0,0 +1,25 @@
+class PatternPrinting {
+    public static void main(String[] args) {
+        int n = 4;
+
+        for (int i = 1; i <= n; i++) {
+
+
+            for (int s = 1; s <= n - i; s++) {
+                System.out.print(" ");
+            }
+
+
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j);
+            }
+
+
+            for (int j = 2; j <= i; j++) {
+                System.out.print(j);
+            }
+
+            System.out.println();
+        }
+    }
+}
