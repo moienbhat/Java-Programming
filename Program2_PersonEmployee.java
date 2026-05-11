7487f44a8aee23f95346c1892a064242deca720a
Moien Bhat <bhatmoien34@gmail.com>
2026-05-11 20:22:08 +0530

    Add files via upload

---
+++ Program2_PersonEmployee.java
@@ -0,0 +1,42 @@
+class Person {
+    String name;
+    int age;
+
+    Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    void display() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
+
+class Employee extends Person {
+    double salary;
+
+    Employee(String name, int age, double salary) {
+        super(name, age);
+        this.salary = salary;
+    }
+
+    @Override
+    void display() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+    }
+}
+
+public class Program2_PersonEmployee {
+    public static void main(String[] args) {
+        Person p = new Person("Ali", 22);
+        p.display();
+
+        System.out.println();
+
+        Employee e = new Employee("Ahmed", 30, 50000);
+        e.display();
+    }
+}
